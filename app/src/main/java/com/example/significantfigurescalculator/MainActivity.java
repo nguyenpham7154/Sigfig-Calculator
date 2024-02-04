af0db52cdc2d4b62868e4b301494097bac4d54b6
Dinh Viet Luong <dinhvietluong99@gmail.com>
2024-02-03 22:08:41 -0500

    Button Sounds Implementation + Code CleanUp

--- app/src/main/java/com/example/significantfigurescalculator/MainActivity.java
+++ app/src/main/java/com/example/significantfigurescalculator/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.significantfigurescalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MainActivity extends AppCompatActivity {
 
+    // Id of all buttons on calculators (Finding id through strings is more work...)
     public static int[] button_id = {
             // Subjects
             R.id.Math, R.id.Biology, R.id.Chemistry, R.id.Physics,
@@ -24,44 +22,61 @@ public class MainActivity extends AppCompatActivity {
             // Custom Functions
             R.id.Function1, R.id.Function2, R.id.Function3, R.id.Function4,
             // Others
-            R.id.plusMinus, R.id.decimal
+            R.id.plusMinus, R.id.decimal, R.id.equal
     };
 
+    // Container of sound files for every elements (0-4 index is for button sounds)
+    public MediaPlayer[] mediaPlayer = new MediaPlayer[4];
+
+    // This function has to be called after the activity is created or the app crashes
+    public void setButtonSound() {
+        mediaPlayer[0] = MediaPlayer.create(this,R.raw.button_press_1);
+        mediaPlayer[1] = MediaPlayer.create(this,R.raw.button_press_2);
+        mediaPlayer[2] = MediaPlayer.create(this,R.raw.button_press_3);
+        mediaPlayer[3] = MediaPlayer.create(this,R.raw.button_press_4);
+    }
+
     public void setButtonFunctions() {
+        // Initializes sound
+        setButtonSound();
+
+        // Sets property for each type of buttons
         for (int id : button_id) {
             Button key = (Button)findViewById(id);
+            key.setSoundEffectsEnabled(false);
 
+            // Attributes function to each buttons
             String keyFunction = (getResources().getResourceEntryName(id));
             switch (keyFunction) {
-                case "Math":
-                case "Biology":
-                case "Chemistry":
-                case "Physics":
+                // Subjects
+                case "Math": case "Biology": case "Chemistry": case "Physics":
                     key.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
+                            mediaPlayer[(int)(Math.random() * 4)].start(); // Play Sound
                             changeSubject((getResources().getResourceEntryName(v.getId())));
                         }
                     });
                     break;
-                case "zero0":
-                case "one1":
-                case "two2":
-                case "three3":
-                case "four4":
-                case "five5":
-                case "six6":
-                case "seven7":
-                case "eight8":
-                case "nine9":
+                // Digits
+                case "zero0": case "one1": case "two2": case "three3": case "four4": case "five5": case "six6": case "seven7": case "eight8": case "nine9":
                     key.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
+                            mediaPlayer[(int)(Math.random() * 4)].start(); // Play Sound
                             String keyFunction = (getResources().getResourceEntryName(v.getId()));
                             addDigit(keyFunction.charAt(keyFunction.length() - 1) - '0');
                         }
                     });
                     break;
+                // Unassigned Buttons
+                default :
+                    key.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mediaPlayer[(int)(Math.random() * 4)].start(); // Play Sound
+                        }
+                    });
             }
 
         }
@@ -81,5 +96,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Sets buttons
+        setButtonFunctions();
     }
 }
